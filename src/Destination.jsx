8bde75cde6cc062abd7f247b007dde9343068951
ViktorChs <Victoraltas123@gmail.com>
2023-10-23 11:25:36 -0400

    1.2

--- src/Destination.jsx
+++ src/Destination.jsx
@@ -22,13 +22,13 @@ function Destination(){
             Description: ' See our planet as you\’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you\’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
             Distance: '384,400 km',
             TravelTime: '3 days',
-            img: "src/destination/image-moon.webp"
+            img: "https://github.com/ViktorChs/Planets/blob/main/src/destination/image-moon.png?raw=true"
         },
         Mars: {
             Description: '  Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
             Distance: '225 mil. km',
             TravelTime: '9 months',
-            img: "src/destination/image-mars.png"
+            img: "https://github.com/ViktorChs/Planets/blob/main/src/destination/image-mars.png?raw=true"
         },
         Europa: {
             Description: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
@@ -40,7 +40,7 @@ function Destination(){
             Description: '  The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
             Distance: '1.6 bil. km',
             TravelTime: '7 years',
-            img: "/src/destination/image-titan.png"
+            img: "https://github.com/ViktorChs/Planets/blob/main/src/destination/image-titan.png?raw=true"
         }
       };
     return(
