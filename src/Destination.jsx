7280014ca49a57ec1e89008757f17f560a66cf52
ViktorChs <Victoraltas123@gmail.com>
2023-10-23 10:38:25 -0400

    08

--- src/Destination.jsx
+++ src/Destination.jsx
@@ -22,7 +22,7 @@ function Destination(){
             Description: ' See our planet as you\’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you\’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
             Distance: '384,400 km',
             TravelTime: '3 days',
-            img: "public/destination/image-moon.webp"
+            img: "assets/destination/image-moon.webp"
         },
         Mars: {
             Description: '  Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
