aa763c510b0d5814e32b19dde9d566165791d3a9
ViktorChs <Victoraltas123@gmail.com>
2023-10-24 13:08:40 -0400

    1.9

--- src/Nav-bar-desktop.jsx
+++ src/Nav-bar-desktop.jsx
@@ -13,7 +13,7 @@ const MenuDesplegable = () => {
 
   return (
     <div className="nav_bar-mobile">
-    <img className='Logo' src="/vercel/path0/src/shared/logo.svg" alt='logo'/>
+    <img className='Logo' src="/src/home/background-home-mobile.jpg" alt='logo'/>
       <button onClick={toggleMenu}><img className='logo' src="src/shared/icon-hamburger.svg" alt="" /></button>
       {menuVisible && (
         <div className='nav_bar-desktop'>
