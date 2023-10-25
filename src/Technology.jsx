a1dffb39234b17b8968c090bfd27bfc6aff6d9da
ViktorChs <Victoraltas123@gmail.com>
2023-10-25 12:49:01 -0400

    2.6

--- src/Technology.jsx
+++ src/Technology.jsx
@@ -1,5 +1,13 @@
 import React, { useState , useEffect} from "react"
 
+import Techimg6 from "/src/technology/image-launch-vehicle-portrait.jpg"
+import Techimg5 from "/src/technology/image-launch-vehicle-landscape.jpg"
+
+import Techimg4 from "/src/technology/image-spaceport-portrait.jpg"
+import Techimg3 from "/src/technology/image-spaceport-landscape.jpg"
+
+import Techimg2 from "/src/technology/image-space-capsule-portrait.jpg"
+import Techimg1 from "/src/technology/image-space-capsule-landscape.jpg"
 import Menu from "./Menu";
 
 
@@ -52,20 +60,20 @@ function Technology (){
         Capsule: {
             Description: 'A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth\'s atmosphere without wings. Our capsule is where you\'ll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.',
             Profession: 'Space capsule',
-            img: "/src/technology/image-space-capsule-portrait.jpg",
-            img2:"/src/technology/image-space-capsule-landscape.jpg"
+            img: {Techimg1},
+            img2:{Techimg2}
         },
         Spaceport: {
             Description: ' A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.',
             Profession: 'Spaceport',
-            img: "/src/technology/image-spaceport-portrait.jpg",
-            img2:"/src/technology/image-spaceport-landscape.jpg"
+            img: {Techimg3},
+            img2:{Techimg4}
         },
         Vehicle: {
             Description: 'A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth\'s surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it\'s quite an awe-inspiring sight on the launch pad!',
             Profession: 'Launch vehicle',
-            img: "/src/technology/image-launch-vehicle-portrait.jpg",
-            img2:"/src/technology/image-launch-vehicle-landscape.jpg"
+            img: {Techimg5},
+            img2:{Techimg6}
         }
       }
       function Appp() {
